30b83cb46d61bdbcf3b81f58ecf072d750eb77c4
Mahesh13247 <139057471+Mahesh13247@users.noreply.github.com>
2026-02-03 18:10:07 +0530

    update 1.1

--- app/layout.tsx
+++ app/layout.tsx
@@ -85,7 +85,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-      
         <ThemeProvider>
           <div className="flex min-h-screen flex-col">
             <Navbar />
