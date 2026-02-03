918cb6d4e7faf08920b86f04646eff86695a1ce3
Mahesh13247 <139057471+Mahesh13247@users.noreply.github.com>
2026-02-03 16:42:19 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -27,8 +27,8 @@ export default function Home() {
             or the{" "}
             <a
               href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
+              className="font-medium text-zinc-950 dark:text-zinc-50">
+            
               Learning
             </a>{" "}
             center.
